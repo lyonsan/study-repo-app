e28d2ca9f545bdb325945f404c43528393f249aa
lyonsan <ryoga.yajima@gmail.com>
2021-01-03 17:26:04 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+=======
+  root to: "rooms#index"
+>>>>>>> Stashed changes
 end
