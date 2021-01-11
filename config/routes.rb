6ded820a23eae1326da73e3036ac8ca98242f7be
lyonsan <ryoga.yajima@gmail.com>
2021-01-11 22:08:45 +0900

    ルーティング、コントローラー設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
-  resources :rooms
+  resources :rooms do
+    resources :reports, only: [:index, :new, :create]
+  end
 end
