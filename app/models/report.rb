8e15c216fba37882c8d8858183b4ad3eb4a57444
lyonsan <ryoga.yajima@gmail.com>
2021-01-12 18:29:55 +0900

    バリデーション仮入力、他モデルアソシエーション

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,12 +2,14 @@ class Report < ApplicationRecord
   belongs_to :user
   belongs_to :room
   # バリデーションを記入する
-  # study_time
-  # concentrated_time
-  # good_way
-  # achieved_or_not
-  # go_wrong
-  # tomorrow_plan
-  # study_content
-  # advice
+  with_options presence: true do
+    validates study_time
+    validates concentrated_time
+    validates good_way
+    validates achieved_or_not
+    validates tomorrow_plan
+    validates study_content
+    validates advice
+  end
+  validates go_wrong, presence: true, if: achieved_or_not == 0
 end
