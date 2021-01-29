a6ce35c803555fb686867f2ca69f55676a5c53a9
lyonsan <ryoga.yajima@gmail.com>
2021-01-29 18:12:49 +0900

    ActionCable AWS環境下での作動

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   ActionCable.server.config.disable_request_forgery_protection = true
-  config.action_cable.url = "wss://study-repo-app.herokuapp.com/cable" 
-  config.action_cable.allowed_request_origins = ['https://study-repo-app.herokuapp.com', 'http://study-repo-app.herokuapp.com']
-
+  config.action_cable.url = "ws://18.176.46.136/cable" 
+  config.action_cable.allowed_request_origins = ['http://18.176.46.136']
   # Code is not reloaded between requests.
   config.cache_classes = true
 
