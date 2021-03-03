e3c994059768aac26c6d229b34d6c885e2bb0dcd
lyonsan <ryoga.yajima@gmail.com>
2021-03-03 21:49:48 +0900

    単体テスト

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :article do
-    
+    association :user
+    summary { Faker::Lorem.sentence }
+    detail { Faker::Lorem.sentence }
+    study_genre_id { Faker::Number.between(from: 2, to: 6) }
   end
 end
