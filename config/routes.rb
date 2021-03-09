071b329c5f51003117bb5d626ce10fabefc70d64
lyonsan <ryoga.yajima@gmail.com>
2021-03-09 20:32:00 +0900

    LP新設

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
+  get "home", to: 'home#index'
   resources :rooms do
     resources :reports, only: [:index, :new, :create, :destroy]
   end
