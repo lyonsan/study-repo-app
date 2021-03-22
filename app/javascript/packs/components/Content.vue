86f805c93f7a1866c224363a036db66cc13ad64f
lyonsan <ryoga.yajima@gmail.com>
2021-03-22 21:47:27 +0900

    ランディングレイアウト修正

--- app/javascript/packs/components/Content.vue
+++ app/javascript/packs/components/Content.vue
@@ -9,52 +9,55 @@
       </div>
       <div class="content-content">
         <div class="report-right-sp">
-          <img src="/images/report_human.jpg" alt="" class="content-pict">
         </div>
         <div class="report-left">
-          <h3>仲間との報告</h3>
-          <p>仲間と学習報告ルームを作成して、日々の学習内容、時間、感じたことを共有できます。</p>
-          <p>周りの目があることで毎日学習をやらざるを得ない環境を作ることができますし、</p>
-          <p>仮に一人でも1日の終わりに学習内容を振り返ることができます。</p>
+          <div class="content-text-wrapper">
+            <h3>仲間との報告</h3>
+            <p>仲間と学習報告ルームを作成して、日々の学習内容、時間、感じたことを共有できます。</p>
+            <p>周りの目があることで毎日学習をやらざるを得ない環境を作ることができますし、</p>
+            <p>仮に一人でも1日の終わりに学習内容を振り返ることができます。</p>
+          </div>
         </div>
         <div class="report-right">
-          <img src="/images/report_human.jpg" alt="" class="content-pict">
         </div>
       </div>
       <div class="content-content">
         <div class="memo-left">
-          <img src="/images/rooms-index-04.jpeg" alt="" class="content-pict">
         </div>
         <div class="memo-right">
-          <h3>学習中の気付きをメモ</h3>
-          <p>学習中に「これは記録しておきたい!」と思ったらすぐにメモできます。</p>
-          <p>ご自分の好きな項目で「メモルーム」を作成していただき、</p>
-          <p>メモルームの中に学習中の気付きをメモしていただけます。</p>
+          <div class="content-text-wrapper">
+            <h3>学習中の気付きをメモ</h3>
+            <p>学習中に「これは記録しておきたい!」と思ったらすぐにメモできます。</p>
+            <p>ご自分の好きな項目で「メモルーム」を作成していただき、</p>
+            <p>メモルームの中に学習中の気付きをメモしていただけます。</p>
+          </div>
         </div>
       </div>
       <div class="content-content">
         <div class="article-right-sp">
-          <img src="/images/article_real.jpg" alt="" class="content-pict">
         </div>
         <div class="article-left">
-          <h3>学んだことを記事にして公開する</h3>
-          <p>学習したことを記事にして公開しましょう。</p>
-          <p>人にわかりやすく伝えることを通して、自分の理解を深めましょう</p>
-          <a href="/articles" class="articles-link">→実際の記事を見てみる</a>
+          <div class="content-text-wrapper">
+            <h3>学んだことを記事にして公開する</h3>
+            <p>学習したことを記事にして公開しましょう。</p>
+            <p>人にわかりやすく伝えることを通して、自分の理解を深めましょう</p>
+            <a href="/articles" class="articles-link">→実際の記事を見てみる</a>
+          </div>
         </div>
         <div class="article-right">
-          <img src="/images/article_real.jpg" alt="" class="content-pict">
         </div>
       </div>
       <div class="content-content">
         <div class="user-left">
-          <img src="/images/user_real.jpg" alt="" class="content-pict">
+          
         </div>
         <div class="user-right">
-          <h3>仲間と繋がる</h3>
-          <p>そして学習をやり切る上で大切なのが学習仲間。</p>
-          <p>登録しているユーザーを検索し、同志を見つけて学習を共に行っていきましょう!</p>
-          <a href="/users" class="articles-link">→ユーザー一覧へ</a>
+          <div class="content-text-wrapper">
+            <h3>仲間と繋がる</h3>
+            <p>そして学習をやり切る上で大切なのが学習仲間。</p>
+            <p>登録しているユーザーを検索し、同志を見つけて学習を共に行っていきましょう!</p>
+            <a href="/users" class="articles-link">→ユーザー一覧へ</a>
+          </div>
         </div>
       </div>
     </div>
