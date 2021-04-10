653a8d46ede6dc610c4e63ac3ced985b0c8adef9
lyonsan <ryoga.yajima@gmail.com>
2021-04-10 14:46:07 +0900

    文言統一

--- spec/system/subjects_spec.rb
+++ spec/system/subjects_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe '科目別メモルームの作成機能', type: :system do
+RSpec.describe '科目ルームの作成機能', type: :system do
   before do
     @user = FactoryBot.create(:user)
     @subject_name = Faker::Team.name
     @subject_purpose_subject = Faker::Lorem.sentence
   end
-  context '科目別メモルーム作成ができる時' do
-    it 'ログインしたユーザーは新規科目別メモルームを作成できる' do
+  context '科目ルーム作成ができる時' do
+    it 'ログインしたユーザーは新規科目ルームを作成できる' do
       # サインインする
       sign_in(@user)
       # メモルーム一覧ページへのリンクがあることを確認する
@@ -17,11 +17,11 @@ RSpec.describe '科目別メモルームの作成機能', type: :system do
       # メモルーム一覧ページに遷移する
       visit subjects_path
       # メモルーム新規作成ページへのリンクがあることを確認する
-      expect(page).to have_content('科目/カテゴリー新規作成')
+      expect(page).to have_content('科目ルーム新規作成')
       # メモルーム新規作成ページに遷移する
       visit new_subject_path
       # フォームに情報を記入する
-      fill_in 'カテゴリ/科目名', with: @subject_name
+      fill_in '科目ルーム名', with: @subject_name
       fill_in '目的/主旨', with: @subject_purpose_subject
       # 科目作成ボタンを押すとSubjectモデルのカウントが1上がる
       expect do
@@ -30,7 +30,7 @@ RSpec.describe '科目別メモルームの作成機能', type: :system do
       # 作成完了ページに遷移したことを確認する
       expect(current_path).to eq subjects_path
       # 「新規ルーム作成が完了しました!」の表示があることを確認する
-      expect(page).to have_content('新規ルーム作成が完了しました!')
+      expect(page).to have_content('新規科目ルーム作成が完了しました!')
       # メモ一覧ページへのリンクがあることを確認する
       expect(page).to have_content('一覧ページに戻る')
       # メモ一覧ページに遷移する
@@ -39,12 +39,12 @@ RSpec.describe '科目別メモルームの作成機能', type: :system do
       expect(page).to have_content(@subject_name)
     end
   end
-  context '科目別メモルームが作成できない時' do
-    it 'ログインしていないユーザーは新規科目別メモルームを作成できない' do
+  context '科目ルームが作成できない時' do
+    it 'ログインしていないユーザーは新規科目ルームを作成できない' do
       # トップページに遷移する
       visit root_path
       # 新規作成ページへのリンクがない
-      expect(page).to have_no_content('科目/カテゴリー新規作成')
+      expect(page).to have_no_content('科目ルーム新規作成')
     end
     it '正しい情報を入力しなければ作成できない' do
       # サインインする
@@ -54,11 +54,11 @@ RSpec.describe '科目別メモルームの作成機能', type: :system do
       # メモルーム一覧ページに遷移する
       visit subjects_path
       # メモルーム新規作成ページへのリンクがあることを確認する
-      expect(page).to have_content('科目/カテゴリー新規作成')
+      expect(page).to have_content('科目ルーム新規作成')
       # メモルーム新規作成ページに遷移する
       visit new_subject_path
       # フォームに誤った情報を記入する
-      fill_in 'カテゴリ/科目名', with: nil
+      fill_in '科目ルーム名', with: nil
       fill_in '目的/主旨', with: nil
       # 科目作成ボタンを押してもSubjectモデルのカウントが上がらない
       expect do
@@ -70,11 +70,11 @@ RSpec.describe '科目別メモルームの作成機能', type: :system do
   end
 end
 
-RSpec.describe '科目別メモルームの削除機能', type: :system do
+RSpec.describe '科目ルームの削除機能', type: :system do
   before do
     @subject = FactoryBot.create(:subject)
   end
-  context '科目別ルーム削除がうまくいく時' do
+  context '科目ルーム削除がうまくいく時' do
     it 'ルーム作成者は科目ルームを削除することができる' do
       # サインインする
       sign_in(@subject.user)
